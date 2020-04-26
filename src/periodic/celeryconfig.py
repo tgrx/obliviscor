e59cbfc2a0390534b71168b202208982ddb56725
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-04-27 02:21:33 +0300

    FIX: redis on heroku

--- src/periodic/celeryconfig.py
+++ src/periodic/celeryconfig.py
@@ -1,6 +1,12 @@
+from os import getenv
+
 from dynaconf import settings
 
-broker_url = settings.CELERY_BROKER_URL
+_broker_url = settings.CELERY_BROKER_URL
+if settings.ENV_FOR_DYNACONF == "heroku":
+    _broker_url = getenv("REDIS_URL")
+
+broker_url = _broker_url
 imports = ["periodic.tasks"]
-result_backend = settings.CELERY_BROKER_URL
+result_backend = _broker_url
 result_persistent = False
